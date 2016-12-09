ae72dde38ba8bf157832538738e768eb7a5e24b9
Lucas Maximiliano Marino <elementh.github@icloud.com>
2016-12-09 15:25:31 +0100

    Working! Next comes some weird css try and error

---
+++ assets/gimmekeycodes.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const selectors = require('./selectors')
+
+function update (e) {
+  changehtml(e.key, e.keyCode)
+}
+
+function changehtml (key, keycode) {
+  selectors.display(keycode)
+  selectors.key(key)
+}
+module.exports = {
+  update: update
+}
---
+++ assets/selectors.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function display(text) {
+  let display = document.querySelector('.display')
+  display.innerHTML = '<h1>' + text + '</1>'
+}
+
+function key(text) {
+  let key = document.querySelector('.key')
+  if (isVowel(text)) {
+    key.innerHTML = '<h3>You pressed an \'' + text + '\' key!</h3>'
+  } else {
+    key.innerHTML = '<h3>You pressed a \'' + text + '\' key!</h3>'
+  }
+}
+
+function isVowel (text) {
+  if (text.charAt(0) === 'a' || text.charAt(0) === 'e' || text.charAt(0) === 'i' || text.charAt(0) === 'o' || text.charAt(0) === 'u') {
+    return true
+  } else {
+    return false
+  }
+}
+
+module.exports = {
+  display: display,
+  key: key
+}
