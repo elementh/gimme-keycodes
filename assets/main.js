d99767299a7ff1bd7e34bc5a279db4902f8802f6
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-15 19:21:00 +0200

    Added metakey prevention

--- assets/main.js
+++ assets/main.js
@@ -3,5 +3,8 @@
 const gimmekeycodes = require('./gimmekeycodes')
 
 document.addEventListener('keydown', function (e) {
+  if (!e.metaKey) {
+    e.preventDefault();
+  }
   gimmekeycodes.update(e)
 })
